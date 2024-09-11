c136ce44554f09c3d535a71c2115a99d811ff830
glorvibop <shaineglorvina@gmail.com>
2024-09-11 14:40:08 +0700

    added CSRF_TRUSTED_ORIGINS in settings.py

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -121,3 +121,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://shaine-glorvina-trackergokil.pbp.cs.ui.ac.id/", "https://shaine-glorvina-trackergokil.pbp.cs.ui.ac.id/U>"]
